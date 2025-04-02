657d506fa8a0b053d4dc0b8275e315e4c829f44e
gepopp <gerhard@poppgerhard.at>
2025-04-02 19:55:12 +0200

    debug

--- resources/views/livewire/parts/landing-hero.blade.php
+++ resources/views/livewire/parts/landing-hero.blade.php
@@ -13,10 +13,15 @@
         </div>
     </div>
     <div @class(['order-first md:order-last -mx-4 md:ml-0 mb-8 relative overflow-hidden'])>
-        <img src="{{ $media->getUrl() }}" srcset="{{ $media->getSrcset() }}" alt="{{ $alt }}" data-aos="fade" data-aos-delay="600" class="h-full w-auto object-cover"/>
+
+        @if(!is_null($media))
+            <img src="{{ $media?->getUrl() }}" srcset="{{ $media?->getSrcset() }}" alt="{{ $alt }}" data-aos="fade"
+                 data-aos-delay="600" class="h-full w-auto object-cover"/>
+        @endif
 
         <div class="absolute top-0 left-0 w-[20%] h-full hidden md:block">
-            <svg @class(['text-white h-full w-auto']) fill="currentColor" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 85.3 501">
+            <svg @class(['text-white h-full w-auto']) fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                 version="1.1" viewBox="0 0 85.3 501">
                 <polygon class="st0" points="0 501 0 501 0 0 85.3 0 0 501"/>
             </svg>
         </div>
