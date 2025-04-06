458a50ef3aa76f49cf29da74cc7a03757ad9fd48
gepopp <gerhard@poppgerhard.at>
2025-04-06 21:59:06 +0200

    debug

--- resources/views/components/dev-switsher.blade.php
+++ resources/views/components/dev-switsher.blade.php
@@ -4,12 +4,13 @@
             <div x-data>
                 <p x-on:click="$flux.dark = ! $flux.dark" class="!text-xs">Dark/Light</p>
             </div>
-            <p class="sm:hidden">xs</p>
-            <p class="hidden sm:block md:hidden">sm</p>
-            <p class="hidden md:block lg:hidden">md</p>
-            <p class="hidden lg:block xl:hidden">lg</p>
-            <p class="hidden xl:block 2xl:hidden">xl</p>
-            <p class="hidden 2xl:block">2xl</p>
+            <span class="sm:hidden">xs</span>
+            <span class="hidden sm:block md:hidden">sm</span>
+            <span class="hidden md:block lg:hidden">md</span>
+            <span class="hidden lg:block xl:hidden">lg</span>
+            <span class="hidden xl:block 2xl:hidden">xl</span>
+            <span class="hidden 2xl:block">2xl</span>
         </div>
     @endenv
+    <div class="leading-[65px]"></div>
 </div>
