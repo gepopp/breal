5a53f2f997a4aae24eece5526300e99011e4949b
gepopp <gerhard@poppgerhard.at>
2025-04-09 07:57:50 +0200

    refactor

---
+++ resources/views/components/section.blade.php
@@ -0,0 +1,5 @@
+<section {{ $attributes->merge([ 'class' => 'py-12 md:py-24 xl:py-36 flex justify-center' ]) }}>
+        <div class="lg:max-w-4xl xl:max-w-6xl  px-4">
+            {{ $slot }}
+        </div>
+</section>
