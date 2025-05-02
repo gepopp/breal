3781263dc396904a0dae4344b81fae4e4219d308
gepopp <gerhard@poppgerhard.at>
2025-05-02 21:48:35 +0200

    wip

--- app/Livewire/Subparts/RealtyThumbnail.php
+++ app/Livewire/Subparts/RealtyThumbnail.php
@@ -32,6 +32,17 @@ class RealtyThumbnail extends Component
         }
     }
 
+    public function placeholder()
+    {
+        return <<<'HTML'
+        <div class="w-24 aspect-video animate-pulse bg-gray-200 flex items-center justify-center">
+            <svg class="animate-spin -ml-1 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+        </div>
+        HTML;
+    }
 
     public function render()
     {
