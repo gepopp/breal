4386788f3db968ff4321eca05df436cb53c2026f
gepopp <gerhard@poppgerhard.at>
2025-05-02 22:23:06 +0200

    debug

--- resources/views/components/headings.blade.php
+++ resources/views/components/headings.blade.php
@@ -4,7 +4,8 @@
 @props([
     'level' => null,
     'tag' => 'real',
-    'ondark' => false
+    'ondark' => false,
+    'animate' => true
 ])
 
 @php
@@ -18,7 +19,8 @@
         "text-technik-500" => Route::is('technik.*') && !$ondark,
         "text-makler-500" => Route::is('makler.*') && !$ondark,
 ])>
-    <div @class([
+    @if($animate)
+        <div @class([
         "flex items-center font-logo text-3xl md:text-4xl lg:text-5xl xl:text-7xl font-extrabold rounded tracking-wide lowercase",
         "before:content-['be'] before:h-[32px] before:mb-[6px] before:md:h-[36px] before:lg:h-[42px] before:xl:h-[62px] before:align-baseline before:rounded before:px-2 before:tracking-wide dark:before:bg-white before:bg-logo dark:before:text-logo before:mr-2",
         "text-logo-500" => !Route::is('technik.*', 'makler.*') && !$ondark,
@@ -30,27 +32,41 @@
         "before:bg-white " => $ondark,
         "before:text-white before:bg-logo" => !$ondark
         ])
-         data-aos="fade" data-aos-once="true">
-        <span class="font-logo dark:text-white">{{$tag}}</span>
-    </div>
-    <?php switch ((int)$level): case(1): ?>
-    <h1 data-aos="fade" data-aos-once="true" data-aos-delay="750" {{ $attributes->class($classes) }}>{{ $slot }}</h1>
+             data-aos="fade" data-aos-once="true">
+            @else
+                <div @class([
+        "flex items-center font-logo text-3xl md:text-4xl lg:text-5xl xl:text-7xl font-extrabold rounded tracking-wide lowercase",
+        "before:content-['be'] before:h-[32px] before:mb-[6px] before:md:h-[36px] before:lg:h-[42px] before:xl:h-[62px] before:align-baseline before:rounded before:px-2 before:tracking-wide dark:before:bg-white before:bg-logo dark:before:text-logo before:mr-2",
+        "text-logo-500" => !Route::is('technik.*', 'makler.*') && !$ondark,
+        "text-technik-500" => Route::is('technik.*') && !$ondark,
+        "text-makler-500" => Route::is('makler.*') && !$ondark,
+        "before:text-logo-500" => !Route::is('technik.*', 'makler.*') && $ondark,
+        "before:text-technik-500" => Route::is('technik.*') && $ondark,
+        "before:text-makler-500 dark:before:text-makler-950" => Route::is('makler.*') && $ondark,
+        "before:bg-white " => $ondark,
+        "before:text-white before:bg-logo" => !$ondark
+        ])>
+                    @endif
+                    <span class="font-logo dark:text-white">{{$tag}}</span>
+                </div>
+                <?php switch ((int)$level): case(1): ?>
+                <h1 data-aos="fade" data-aos-once="true" data-aos-delay="750" {{ $attributes->class($classes) }}>{{ $slot }}</h1>
 
-    @break
-    <?php case(2): ?>
-    <h2 {{ $attributes->class($classes) }} data-aos-once="true" data-aos="fade" data-aos-delay="500" data-flux-heading>{{ $slot }}</h2>
+                @break
+                <?php case(2): ?>
+                <h2 {{ $attributes->class($classes) }} data-aos-once="true" data-aos="fade" data-aos-delay="500" data-flux-heading>{{ $slot }}</h2>
 
-    @break
-    <?php case(3): ?>
-    <h3 {{ $attributes->class($classes) }} data-aos-once="true" data-aos="fade" data-aos-delay="500">{{ $slot }}</h3>
+                @break
+                <?php case(3): ?>
+                <h3 {{ $attributes->class($classes) }} data-aos-once="true" data-aos="fade" data-aos-delay="500">{{ $slot }}</h3>
 
-    @break
-    <?php case(4): ?>
-    <h4 {{ $attributes->class($classes) }} data-aos-once="true" data-aos="fade" data-aos-delay="500">{{ $slot }}</h4>
+                @break
+                <?php case(4): ?>
+                <h4 {{ $attributes->class($classes) }} data-aos-once="true" data-aos="fade" data-aos-delay="500">{{ $slot }}</h4>
 
-    @break
-    <?php default: ?>
-    <div {{ $attributes->class($classes) }} data-aos-once="true" data-aos="fade" data-aos-delay="500">{{ $slot }}</div>
-    <?php endswitch; ?>
-</div>
+                @break
+                <?php default: ?>
+                <div {{ $attributes->class($classes) }} data-aos-once="true" data-aos="fade" data-aos-delay="500">{{ $slot }}</div>
+                <?php endswitch; ?>
+        </div>
 
--- resources/views/livewire/pages/immobiliensuche.blade.php
+++ resources/views/livewire/pages/immobiliensuche.blade.php
@@ -1,11 +1,11 @@
 <x-section>
 
     <div @class([ 'max-w-screen w-full', 'md:max-w-sm lg:max-w-lg' => !is_array($preparedText)])>
-        <x-headings>
+        <x-headings :animate="false">
             <x-slot name="tag">{{ $settings->search_header }}</x-slot>
             {!! $settings->search_subheader !!}
         </x-headings>
-        <div data-aos="fade" data-aos-delay="600" class="prose max-w-full">
+        <div class="prose max-w-full">
 
             @if(!is_array($preparedText))
                 {!! html_entity_decode( $preparedText ) !!}
