9d9c275cda374f87284a24c439b9b15aa047f748
gepopp <gerhard@poppgerhard.at>
2025-05-12 13:43:45 +0200

    wip

--- resources/views/livewire/pages/compentence.blade.php
+++ resources/views/livewire/pages/compentence.blade.php
@@ -14,10 +14,10 @@
 
 
 
-        <div class="w-full" data-aos="fade-up" data-aos-delay="200">
-            <div class="w-1/2 float-right ml-8 relative">
+        <div class="w-full mt-8" data-aos="fade-up" data-aos-delay="200">
+            <div class="w-full mb-8 sm:mb-0 sm:w-1/2 float-right ml-8 relative">
                 <img src="{{ $competence->getFirstMediaUrl('titleimage', 'article_header') }}" class="w-full aspect-video object-cover" alt="{{ $competence->name }}"/>
-                <div class="absolute bottom-0 left-0 w-[20%] h-full hidden md:block">
+                <div class="absolute bottom-0 left-0 w-[20%] h-full hidden sm:block">
                     <svg @class(['text-white dark:text-logo-950 h-full w-auto z-[9999] relative']) fill="currentColor" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 85.3 501">
                         <polygon points="0 501 0 501 0 0 85.3 0 0 501"/>
                     </svg>
