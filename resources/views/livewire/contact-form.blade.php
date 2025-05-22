8b9413d87892cee619b5ab9c9b7a9fc866ca194f
gepopp <gerhard@poppgerhard.at>
2025-05-22 12:40:09 +0200

    wip

--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -98,8 +98,8 @@
                     <div class="max-w-xs text-center">
                         <h5 class="text-2xl font-bold">Vielen Dank!</h5>
                         <p class="!text-sm font-thin">
-                            Bitte prüfen Sie ihren Posteingang,
-                            wir haben Ihnen ein E-Mail zum Bestätigen Ihrer Anmeldung gesendet.
+                            Damit wir Ihre Anfrage bearbeiten können, bestätigen Sie bitte Ihre E-Mail-Adresse.
+                            Dazu haben wir Ihnen eine E-Mail mit einem Bestätigungslink gesendet.
                         </p>
                     </div>
                 </div>
