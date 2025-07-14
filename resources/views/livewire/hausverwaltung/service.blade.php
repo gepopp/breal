7f01d22ab1010670e9ed1e0c48fc0515e9a338c2
gepopp <gerhard@poppgerhard.at>
2025-07-14 13:29:09 +0200

    debug

--- resources/views/livewire/hausverwaltung/service.blade.php
+++ resources/views/livewire/hausverwaltung/service.blade.php
@@ -39,8 +39,8 @@
     </section>
 
 
-    <section class="mt-12 flex space-x-8" x-data="siteServiceFilter">
-        <div class="relative  min-h-screen w-full md:w-84 shrink-0">
+    <section class="mt-12 flex flex-col md:flex-row space-x-8" x-data="siteServiceFilter">
+        <div class="relative  md:min-h-screen w-full md:w-84 shrink-0">
             <div class="absolute inset-0 bg-white flex flex-col">
                 <flux:input x-model="searchterm" class="border-logo" icon:trailing="funnel" placeholder="Filtern Sie aus {{ count($services) }} Services"/>
                 <div class="mt-4 pt-4 flex-grow relative">
