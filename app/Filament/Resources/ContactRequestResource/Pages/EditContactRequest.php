a3857730fe3ed5896dc31fbd023bf8ca6dfd4f97
gepopp <gerhard@poppgerhard.at>
2025-09-15 19:59:18 +0200

    mail sending im Backend
    Zeit gebraucht: 12 minutes

--- app/Filament/Resources/ContactRequestResource/Pages/EditContactRequest.php
+++ app/Filament/Resources/ContactRequestResource/Pages/EditContactRequest.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\ContactRequestResource\Pages;
 
 use App\Filament\Resources\ContactRequestResource;
+use App\Livewire\ContactForm;
 use Filament\Actions;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Mail;
 
 class EditContactRequest extends EditRecord
 {
@@ -14,6 +17,18 @@ class EditContactRequest extends EditRecord
     {
         return [
             Actions\DeleteAction::make(),
+            Actions\Action::make('Send Email')
+                ->icon('heroicon-o-envelope')
+                ->form([
+                    TextInput::make('email')
+                        ->label('Email')
+                        ->required()
+                        ->email()
+                        ->default(ContactForm::RECIPIENT)
+                ])
+                ->action(function ($record, array $data) {
+                    Mail::to($data['email'])->send(new \App\Mail\NewContactRequestAdminMail($record));
+                })
         ];
     }
 }
