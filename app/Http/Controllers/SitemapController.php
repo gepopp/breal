20947118c4c360aafab06dd00386371ba7d45bb0
gepopp <gerhard@poppgerhard.at>
2026-02-25 19:19:11 +0100

    wip
    Zeit gebraucht: 25 seconds

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Spatie\Sitemap\SitemapGenerator;
 
 class SitemapController extends Controller
 {
     public function generate()
     {
+        File::delete(public_path('sitemap.xml'));
         SitemapGenerator::create(route('hausverwaltung.home'))->writeToFile(public_path('sitemap.xml'));
     }
 }
