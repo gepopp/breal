02adc24d1f0302f903497cdf9a0c172dbc904a89
gepopp <gerhard@poppgerhard.at>
2026-03-10 19:46:51 +0100

    wip
    Zeit gebraucht: 5 minutes

--- resources/views/livewire/pages/f-a-q.blade.php
+++ resources/views/livewire/pages/f-a-q.blade.php
@@ -28,7 +28,7 @@
         @foreach($faqs as $index => $faq)
             <div class="py-4 border-b border-logo">
                 <p class="!text-lg !font-bold flex justify-between items-center cursor-pointer !text-logo !dark:text-white" x-on:click="current = {{ $index }}">
-                    <span class="dark:text-white">{{ blank($faq['question'][app()->getLocale()]) ? $faq['question']['de'] : $faq['question'][app()->getLocale()] }}</span>
+                    <span class="dark:text-white">{{ blank($faq['question'][app()->getLocale() ?? 'de']) ? $faq['question']['de'] : $faq['question'][app()->getLocale() ?? 'de'] }}</span>
                     <span>
                         <template x-if="current != {{ $index }}">
                              <svg class="size-8" data-slot="icon" fill="none" stroke-width="4" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
@@ -45,7 +45,7 @@
                 </p>
                 <div x-show="current == {{ $index }}" x-collapse>
                     <div class="prose">
-                        {!! blank($faq['answer'][app()->getLocale()]) ? $faq['answer']['de'] : $faq['answer'][app()->getLocale()] !!}
+                        {!! blank($faq['answer'][app()->getLocale() ?? 'de']) ? $faq['answer']['de'] : $faq['answer'][app()->getLocale() ?? 'de'] !!}
                     </div>
                     <div class="h-px w-full bg-logo mt-8"></div>
                     <div class="prose bg-logo p-4 text-white flex flex-col items-center justify-center">
