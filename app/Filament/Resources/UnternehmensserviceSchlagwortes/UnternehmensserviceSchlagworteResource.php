174637f427a6bdebc6fdfa9f4eb71adbfc68c3f5
gepopp <gerhard@poppgerhard.at>
2026-03-30 16:48:24 +0200

    wip
    Zeit gebraucht: 2 minutes

--- app/Filament/Resources/UnternehmensserviceSchlagwortes/UnternehmensserviceSchlagworteResource.php
+++ app/Filament/Resources/UnternehmensserviceSchlagwortes/UnternehmensserviceSchlagworteResource.php
@@ -15,12 +15,15 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class UnternehmensserviceSchlagworteResource extends Resource
 {
     protected static ?string $model = UnternehmensserviceSchlagworte::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::Tag;
+
+    protected static string|UnitEnum|null $navigationGroup = 'Zweisprachige Datenmodelle';
 
     protected static ?string $recordTitleAttribute = 'name';
 
