0900c1351e6b62375b6fe9d029eb0174ef4fc8da
gepopp <gerhard@poppgerhard.at>
2026-05-22 11:45:42 +0200

    wip
    Zeit gebraucht: 7 minutes

--- app/Observers/ServiceObserver.php
+++ app/Observers/ServiceObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Service;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class ServiceObserver
@@ -29,10 +30,16 @@ class ServiceObserver
         $links = [];
 
         foreach($service->links as $link){
+
+            Log::alert('Link:', $link);
+            $url = Storage::disk('hetzner')->url($link['path']);
+            Log::alert('URL:', $url);
+
+
             $links[] = [
               'name' => $link['name'],
               'path' => $link['path'],
-              'url'  => Storage::disk('hetzner')->url($link['path']),
+              'url'  => $url,
             ];
         }
 
